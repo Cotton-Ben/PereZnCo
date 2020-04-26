23d2e6895b43e1850f658a3c92e503736cf41f3b
cotton-ben <ben.cotton@rutgers.edu>
2020-04-26 10:39:36 -0400

    data added in h2

--- src/main/java/com/perez/rest/model/Alien.java
+++ src/main/java/com/perez/rest/model/Alien.java
@@ -8,19 +8,28 @@ public class Alien {
     @Id
     private int aid;
     private String aname;
+    private String tech;
 
     @Override
     public String toString() {
         return "Alien{" +
                 "aid=" + aid +
                 ", aname='" + aname + '\'' +
+                ", tech='" + tech + '\'' +
                 '}';
     }
 
 
-
     public int getAid() {
         return aid;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
     }
 
     public void setAid(int aid) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-insert into alien values (101,'buddy');
+insert into alien values (101,'buddy','Java');
+insert into alien values (102,'jeremy','Spanish');
+insert into alien values (103,'margee','English');
+insert into alien values (104,'clark','music');
+insert into alien values (105,'miah','bitch');
